d9036c4a7bbd2742bd7409622ba02e25d6a055b1
R0Y4L <royalb9514@gmail.com>
2023-03-14 13:44:17 +0400

    Last

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,54 +21,60 @@ using System.Threading;
 using System.Collections;
 using System.IO;
 using System.Security.Cryptography;
+using System.Numerics;
 
-namespace WpfApp1
+namespace Client;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    IPEndPoint? clientEP = null;
+    IPEndPoint? server = null;
+    UdpClient? client = null;
+    public MainWindow()
     {
-        IPEndPoint? clientEP = null;
-        IPEndPoint? server = null;
-        UdpClient? client = null;
-        public MainWindow()
+        InitializeComponent();
+
+        clientEP = new IPEndPoint(IPAddress.Parse("127.1.1.1"), 12);
+        server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
+        client = new UdpClient(clientEP);
+    }
+
+    private async void btn_Click(object sender, RoutedEventArgs e)
+    {
+        var pulse = Encoding.Default.GetBytes("hello");
+
+        if(client != null && server != null)
         {
-            InitializeComponent();
+            await client.SendAsync(pulse, pulse.Length, server);
 
-            clientEP = new IPEndPoint(IPAddress.Parse("127.1.1.1"), 12);
-            server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
-            client = new UdpClient(clientEP);
-        }
-
-        private async void btn_Click(object sender, RoutedEventArgs e)
-        {
-            var pulse = Encoding.Default.GetBytes("hello");
-
-            if(client != null)
+            try
             {
-                await client.SendAsync(pulse, pulse.Length, server);
+                btn.IsEnabled = false;
 
                 UdpReceiveResult rec_lenght = await client.ReceiveAsync();
                 UdpReceiveResult rec_wholeNum = await client.ReceiveAsync();
 
-                int lenght = int.Parse(Encoding.Default.GetString(rec_lenght.Buffer)), 
-                    wholeNum = int.Parse(Encoding.Default.GetString(rec_wholeNum.Buffer)),
-                    len = 0, a = 0;
-
-                byte[] arr = new byte[lenght];
-                List<byte[]> bytes = new List<byte[]>();
-
-                for (int i = 0; i < wholeNum; i++)
+                byte[] arr = await Task.Run<byte[]>(async () =>
                 {
-                    UdpReceiveResult rec_Bytes = await client.ReceiveAsync();
-                    len += rec_Bytes.Buffer.Length;
-                    bytes.Add(rec_Bytes.Buffer);
-                }
+                    int lenght = int.Parse(Encoding.Default.GetString(rec_lenght.Buffer)),
+                        wholeNum = int.Parse(Encoding.Default.GetString(rec_wholeNum.Buffer)),
+                        len = 0, a = 0;
 
-                foreach (var item in bytes)
-                    for (int i = 0; i < item.Length; i++)
-                        arr[a++] = item[i];
+                    byte[] arr = new byte[lenght];
+                    List<byte[]> bytes = new List<byte[]>();
+
+                    for (int i = 0; i < wholeNum; i++)
+                    {
+                        UdpReceiveResult rec_Bytes = await client.ReceiveAsync();
+                        len += rec_Bytes.Buffer.Length;
+                        bytes.Add(rec_Bytes.Buffer);
+                    }
+
+                    foreach (var item in bytes)
+                        for (int i = 0; i < item.Length; i++)
+                            arr[a++] = item[i];
+                    return arr;
+                });
                 try
                 {
                     BitmapImage bitmap = new BitmapImage();
@@ -80,11 +86,20 @@ namespace WpfApp1
                     img.Source = bitmap;
 
                 }
-                catch (ArgumentException)
+                catch (ArgumentException ex)
                 {
-                    // The byte array does not contain valid image data
+                    MessageBox.Show(ex.Message);
                 }
             }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                btn.IsEnabled = true;
+            }
+
         }
     }
 }
